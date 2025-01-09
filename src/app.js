ba8bebf60cfc438bc934eb16d7c666099a7587fc
jiannisspanos002 <jiannisspanos002@gmail.com>
2025-01-09 22:15:02 +0200

    update

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ import Reading from "./modules/readFile.mjs";
 const postRequest = new PostRequest();
 const reading = new Reading();
 const writeFile = new WriteFile();
-writeFile.writeFileMethod(
-  "",
-  "./paok.json"
-);
+// writeFile.writeFileMethod(
+//   "",
+//   "./paok.json"
+// );
 postRequest.PostRequest("proxy.sm.prod.skill90.io");
 
-//reading.readFile()
+reading.readFile()
