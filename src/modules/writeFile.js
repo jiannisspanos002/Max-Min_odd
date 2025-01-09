dc0e3b8f4430d3de99ded6b4520224833ad3c8fd
jiannisspanos002 <jiannisspanos002@gmail.com>
2025-01-09 18:02:50 +0200

    Create writeFile.js

---
+++ src/modules/writeFile.js
@@ -0,0 +1,30 @@
+const https = require("https");
+const fs = require("fs");
+class WriteFile{
+
+    constructor(){}
+
+    writeFileMethod(url,filePath){
+    https
+      .get(url, (response) => {
+        if (response.statusCode === 200) {
+          const file = fs.createWriteStream(filePath);
+          response.pipe(file);
+    
+          file.on("finish", () => {
+            file.close();
+            console.log(`File downloaded successfully: ${filePath}`);
+          });
+        } else {
+          console.error(
+            `Failed to download file. Status code: ${response.statusCode}`
+          );
+        }
+      })
+      .on("error", (err) => {
+        console.error("Error downloading file:", err.message);
+      });
+    }
+
+}
+export default WriteFile
